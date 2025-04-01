a0286d5ce709f94dd825911a0abf060f6fa0ca5c
etaagen <et395@cornell.edu>
2025-04-01 14:44:55 -0700

    cleaning portfolio

--- test/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# import data
-penguins = sns.load_dataset("penguins")
-print(penguins.head())
-print("Ellie is learning Python!")
